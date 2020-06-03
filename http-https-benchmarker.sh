2496441bd5983757a41c8386c20c6e0f34ee7b3f
Yoonje Choi <elxm6123@gmail.com>
2020-06-03 18:18:59 +0900

    Update http-https-benchmarker.sh

--- http-https-benchmarker.sh
+++ http-https-benchmarker.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-        echo " $ ./http-https-benchmarker.sh [address]"
-        exit 1;
-fi
-
 # compile
 gcc -o http_client http_client.c
 gcc -c -o https_client.o https_client.c
 gcc -o https_client https_client.o -lssl -lcrypto
 
 # http
-StartTime=$(date +%s)
-http_client $1
-EndTime=$(date +%s)
-echo "HTTP takes $(($EndTime - $StartTime)) seconds to complete this task."
+time ./http_client $1
 
 # https
-StartTime=$(date +%s)
-https_client $1
-EndTime=$(date +%s)
-echo "HTTPS takes $(($EndTime - $StartTime)) seconds to complete this task."
+time ./https_client $1
